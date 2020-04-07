53a60911ff46e2ae1af2b09a665a1f8b981f90cb
Blade Barringer <blade.barringer@getbraintree.com>
2020-04-07 16:55:40 +0000

    Fixup exports

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,7 @@
-import number from "./card-number";
-import expirationDate from "./expiration-date";
-import expirationMonth from "./expiration-month";
-import expirationYear from "./expiration-year";
-import cvv from "./cvv";
-import postalCode from "./postal-code";
-import creditCardType = require("credit-card-type");
-
-export = {
-  number,
-  expirationDate,
-  expirationMonth,
-  expirationYear,
-  cvv,
-  postalCode,
-  creditCardType,
-};
+export { default as number } from "./card-number";
+export { default as expirationDate } from "./expiration-date";
+export { default as expirationMonth } from "./expiration-month";
+export { default as expirationYear } from "./expiration-year";
+export { default as cvv } from "./cvv";
+export { default as postalCode } from "./postal-code";
+export * as creditCardType from "credit-card-type";
