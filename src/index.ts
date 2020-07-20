41a51edfb345905a940633432d2bcdf33be3d072
Leonardo Matos <leomp120894@gmail.com>
2020-07-20 19:19:14 -0300

    style: ts code style fix

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import * as creditCardType from "credit-card-type"
+import * as creditCardType from "credit-card-type";
 
-import { cardNumber as number } from "./card-number"
-import { expirationDate } from "./expiration-date"
-import { expirationMonth } from "./expiration-month"
-import { expirationYear } from "./expiration-year"
-import { cvv } from "./cvv"
-import { postalCode } from "./postal-code"
+import { cardNumber as number } from "./card-number";
+import { expirationDate } from "./expiration-date";
+import { expirationMonth } from "./expiration-month";
+import { expirationYear } from "./expiration-year";
+import { cvv } from "./cvv";
+import { postalCode } from "./postal-code";
 
 const cardValidator = {
   creditCardType,
@@ -15,9 +15,9 @@ const cardValidator = {
   expirationYear,
   cvv,
   postalCode
-}
+};
 
-export default cardValidator
+export default cardValidator;
 
 export {
   creditCardType,
@@ -27,4 +27,4 @@ export {
   expirationYear,
   cvv,
   postalCode
-}
+};
